4c85db5930d1d402539be0b670bcd411fe5686d1
Kihong <kks4517@naver.com>
2023-11-16 09:02:29 +0900

    create project

---
+++ src/main/java/com/dfo/dunsee/DunseeApplication.java
@@ -0,0 +1,13 @@
+package com.dfo.dunsee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DunseeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DunseeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/dfo/dunsee/DunseeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dfo.dunsee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DunseeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
